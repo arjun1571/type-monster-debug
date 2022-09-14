0e3ec3439f2894d7087213b0f4f2f668d47bf620
Arjun Das <arjundasback515@gmail.com>
2022-09-15 00:48:22 +0600

    mobile view added done

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ const gameOver = () => {
   resultModal.innerHTML += `
     <h1>Finished!</h1>
     <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
-    <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
+    <p>You made <span class="bold-red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
 
